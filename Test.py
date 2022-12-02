66d67394fe902a8a43328e3eae5192f58ea6c9a5
lkoh <lkoh@sierrawireless.com>
2022-12-02 15:02:24 +0800

    Trying to fix Paramkio ssh prob

--- Test.py
+++ Test.py
@@ -30,7 +30,7 @@ def get_testbed_ip(id):
 def get_test_report(ip_addr, time, remote_path, version_name):
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(ip_addr, username='vagrant', password='vagrant')
+    ssh_client.connect('10.29.51.20', username='lkoh', password='12345678')
 
     ftp_client=ssh_client.open_sftp()
 
